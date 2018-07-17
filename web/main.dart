92e30eedfd5b80903d8320e3d72ca0836d8db950
ChacaS0 <bastide.seb.pro@gmail.com>
2018-07-17 13:35:32 +0400

    clone quickstart project

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,6 @@
 import 'package:angular/angular.dart';
-import 'package:kel_kannes/app_component.dart';
+import 'package:angular_app/app_component.template.dart' as ng;
 
 void main() {
-  bootstrap(AppComponent);
+  runApp(ng.AppComponentNgFactory);
 }
