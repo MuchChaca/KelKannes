8edf87ad33db9ab2779b5190d27721d848147096
ChacaS0 <bastide.seb.pro@gmail.com>
2018-07-18 10:26:47 +0400

    :heavy_check_mark:		CRUD for Ticket generated

---
+++ src/Form/TicketType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ticket;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TicketType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('numero')
+            ->add('date_depot')
+            ->add('weight')
+            ->add('unit')
+            ->add('coupeur')
+            ->add('chargeur')
+            ->add('chauffeur')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ticket::class,
+        ]);
+    }
+}
