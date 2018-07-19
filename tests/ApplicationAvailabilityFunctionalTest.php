c176e779b055b1ff9a24cb0b31ac8778f2cc2a5d
ChacaS0 <bastide.seb.pro@gmail.com>
2018-07-19 08:25:21 +0400

    :wrench:		Travis-ci update

--- tests/ApplicationAvailabilityFunctionalTest.php
+++ tests/ApplicationAvailabilityFunctionalTest.php
@@ -9,27 +9,27 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
  */
 class ApplicationAvailabilityFunctionalTest extends WebTestCase
 {
-	 /**
+	/**
 	  * @dataProvider urlProvider
-	  */
-	 public function testPageIsSuccessful($url)
-	 {
-		  $client = self::createClient();
-		  $client->request('GET', $url);
+	 */
+	public function testPageIsSuccessful($url)
+	{
+		$client = self::createClient();
+		$client->request('GET', $url);
 
-		  $tst = $client->getResponse()->isSuccessful() || $client->getResponse()->isRedirect();
-		  $this->assertTrue($tst);
-	 }
+		$tst = $client->getResponse()->isSuccessful() || $client->getResponse()->isRedirect();
+		$this->assertTrue($tst);
+	}
 
-	 public function urlProvider()
-	 {
-		 yield ['/'];
-		 yield ['/login'];
-		 yield ['/register'];
-		 yield ['/admin'];
-		 yield ['/admin/ticket'];
-		 yield ['/admin/user'];
+	public function urlProvider()
+	{
+		yield ['/'];
+		yield ['/login'];
+		yield ['/register'];
+		yield ['/admin'];
+		yield ['/admin/ticket'];
+		yield ['/admin/user'];
 		// ...
-	 }
-	 
+	}
+
 }
